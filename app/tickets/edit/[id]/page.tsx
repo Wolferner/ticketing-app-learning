ac0c57796283d499fec121f1effde5e385a810a2
Wolfern <maksim.scerbuk@gmail.com>
2024-03-25 18:24:57 +0200

    small fix with dynamic route

--- app/tickets/edit/[id]/page.tsx
+++ app/tickets/edit/[id]/page.tsx
@@ -6,7 +6,9 @@ interface Props {
 	params: { id: string };
 }
 
-const TicketForm = dynamic(() => import('@/components/TicketForm'));
+const TicketForm = dynamic(() => import('@/components/TicketForm'), {
+	ssr: false,
+});
 
 const EditTicket = async ({ params }: Props) => {
 	const ticket = await prisma?.ticket.findUnique({
