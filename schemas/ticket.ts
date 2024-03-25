d1574e470ce3db6aeb1508730bedfb5cc7028e7c
Wolfern <maksim.scerbuk@gmail.com>
2024-03-25 15:08:23 +0200

    15. Added zod schema and implemented POST API route

---
+++ schemas/ticket.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const ticketSchema = z.object({
+	title: z.string().min(1, 'Title must be at least 1 character long').max(255),
+	description: z
+		.string()
+		.min(1, 'Description must be at least 1 character long')
+		.max(65535),
+	status: z
+		.string()
+		.min(1, 'Status must be at least 1 character long')
+		.optional(),
+	priority: z
+		.string()
+		.min(1, 'Priority must be at least 1 character long')
+		.max(10)
+		.optional(),
+});
