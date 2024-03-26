cef3bb60ff595c003fcfa0580428486c2a990935
Wolfern <maksim.scerbuk@gmail.com>
2024-03-26 17:38:59 +0200

    37. Implemented user data table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,14 @@
 import UserForm from '@/components/UserForm';
+import prisma from '@/prisma/db';
 import React from 'react';
+import DataTableSimple from './data-table-simple';
 
-const Users = () => {
+const Users = async () => {
+	const users = await prisma.user.findMany();
 	return (
 		<div>
 			<UserForm />
+			<DataTableSimple users={users} />
 		</div>
 	);
 };
